3f79f60186e06ed3b1c1a347c27bc4b1ef9b5437
jakub.sevcik <jakub.sevcik@kiwi.com>
2022-09-25 22:33:03 +0200

    bluelinky

---
+++ pages/api/odometer.ts
@@ -0,0 +1,30 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import BlueLinky from "bluelinky";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const credentials = JSON.parse(process.env.BLUELINK_CREDENTIALS);
+  const { id } = req.query;
+  //@ts-ignore
+  const car = credentials[id];
+  if (!id || !car) {
+    return res.status(500).json("Wrong id param!");
+  }
+  const myPromise = new Promise((resolve, reject) => {
+    const client = new BlueLinky(car.cred);
+
+    client.on("ready", async () => {
+      const vehicle = await client.getVehicle(car.vim);
+      const odometer = await vehicle.odometer();
+      resolve(odometer.value);
+    });
+
+    client.on("error", async (err) => {
+      console.log("bluelink ~ error", err.message);
+      reject(err.message);
+    });
+  });
+  await myPromise
+    .then((val) => res.status(200).json(val))
+    .catch((err) => res.status(500).json(err));
+};
